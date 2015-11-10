1bb81e14daccdcf6c605fe5b814f970a37e57de2
Pavel Fatin <pavel.fatin@jetbrains.com>
2015-11-10 18:51:47 +0100

    Combine SBT + plugin + launcher in a single JAR

--- build.sbt
+++ build.sbt
@@ -1,19 +1,37 @@
-lazy val sbtProjectModel = (project in file("sbt-project-model"))
-  .settings(sbtPlugin := true, scalaVersion := "2.10.5")
+lazy val sbtProjectModel =
+  (project in file("sbt-project-model"))
+    .settings(sbtPlugin := true)
 
-lazy val ideaProjectModel = (project in file("idea-project-model"))
-  .settings(
-    scalaVersion := "2.10.5",
-    libraryDependencies += "com.novocode" % "junit-interface" % "0.11" % Test
-  )
+lazy val ideaProjectModel =
+  (project in file("idea-project-model"))
+    .settings(libraryDependencies += "com.novocode" % "junit-interface" % "0.11" % Test)
 
-lazy val root = (project in file(".")).
-  dependsOn(ideaProjectModel, sbtProjectModel).
-  settings(
-          name := "sbt-intellij",
-          version := "1.0",
-          sbtPlugin := true,
-          scalaVersion := "2.10.5",
-          libraryDependencies += "com.novocode" % "junit-interface" % "0.11" % Test
-  )
+// sbt-launch scope has to be Compile instead of Runtime to be included in the assembly
+lazy val launcher =
+  (project in file("launcher"))
+    .settings(
+      autoScalaLibrary := false,
+      libraryDependencies += "com.typesafe.sbt" % "sbt-launcher" % "0.13.6" % Compile
+    )
 
+// launcher has to be declared as dependency to be included in the assembly
+lazy val root =
+  (project in file(".")).
+    dependsOn(ideaProjectModel, sbtProjectModel, launcher).
+    settings(
+      name := "sbt-intellij",
+      version := "1.0",
+      sbtPlugin := true,
+      libraryDependencies += "com.novocode" % "junit-interface" % "0.11" % Test
+    )
+
+mainClass in assembly := Some("org.jetbrains.sbt.Launcher")
+
+assemblyOption in assembly := (assemblyOption in assembly).value.copy(includeScala = false)
+
+assemblyMergeStrategy in assembly := {
+  case PathList("sbt", "sbt.plugins", xs @ _*) => MergeStrategy.discard
+  case x =>
+    val delegate = (assemblyMergeStrategy in assembly).value
+    delegate(x)
+}
---
+++ launcher/src/main/java/org/jetbrains/sbt/Launcher.java
@@ -0,0 +1,52 @@
+package org.jetbrains.sbt;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+/**
+ * @author Pavel Fatin
+ */
+public class Launcher {
+    private static final String SBT_LAUNCHER_MAIN_CLASS = "xsbt.boot.Boot";
+    private static final String CONVERTER_CLASS_PREFIX = "org.jetbrains.";
+
+    public static void main(String[] args) throws Exception {
+        URL thisJar = Launcher.class.getProtectionDomain().getCodeSource().getLocation();
+        String thisJarPath = new File(thisJar.toURI().getPath()).getPath();
+
+        if (!thisJarPath.endsWith(".jar")) {
+            System.err.println("Launcher must be run as a jar file.");
+            System.exit(-1);
+        }
+
+        ClassLoader parentClassLoader = ClassLoader.getSystemClassLoader().getParent();
+
+        URLClassLoader classLoader = new URLClassLoader(new URL[]{thisJar}, parentClassLoader) {
+            @Override
+            protected Class findClass(String name) throws ClassNotFoundException {
+                if (name.startsWith(CONVERTER_CLASS_PREFIX)) {
+                    throw new ClassNotFoundException(name);
+                }
+                return super.findClass(name);
+            }
+
+            @Override
+            protected Class loadClass(String name, boolean resolve) throws ClassNotFoundException {
+                if (name.startsWith(CONVERTER_CLASS_PREFIX)) {
+                    throw new ClassNotFoundException(name);
+                }
+
+                return super.loadClass(name, resolve);
+            }
+        };
+
+        Class sbtLauncherClass = Class.forName(SBT_LAUNCHER_MAIN_CLASS, true, classLoader);
+        Method method = sbtLauncherClass.getMethod("main", String[].class);
+
+        String command = ";apply -cp " + thisJarPath + " org.jetbrains.sbt.ConverterPlugin ;intellij-read";
+
+        method.invoke(null, new Object[]{new String[]{command}});
+    }
+}
