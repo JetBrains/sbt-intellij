b84f01bb444d1a64e9fa7fba8f2d2a4f187b272b
Pavel Fatin <pavel.fatin@jetbrains.com>
2015-11-13 18:34:53 +0100

    XML: Add trailing space in leaf elements

--- idea-project-model/src/main/scala/org/jetbrains/idea/XML.scala
+++ idea-project-model/src/main/scala/org/jetbrains/idea/XML.scala
@@ -2,7 +2,7 @@ package org.jetbrains.idea
 
 import java.io.{BufferedWriter, File, FileOutputStream, OutputStreamWriter}
 
-import scala.xml.{Node, PrettyPrinter}
+import scala.xml.{Elem, NamespaceBinding, Node, PrettyPrinter}
 
 /**
   * @author Pavel Fatin
@@ -12,7 +12,26 @@ private object XML {
 
   // PrettyPrinter relies on hardcoded '\n' line separator - exactly what is needed,
   // because IDEA always uses this line separator in config files, regardless of OS.
-  private val printer = new PrettyPrinter(180, 2)
+  private val printer = new PrettyPrinter(180, 2) {
+    // Add trailing space in leaf elements
+    // (on a par with IDEA, to make file comparison easier).
+    override protected def leafTag(n: Node) = {
+      val builder = new StringBuilder()
+      builder.append('<')
+      n.nameToString(builder)
+      n.attributes.buildString(builder)
+      builder.append(" />")
+      builder.toString
+    }
+
+    // https://stackoverflow.com/questions/6044883/scala-xml-prettyprinter-to-format-shorter-node-when-there-is-no-text-in-it
+    override protected def traverse(node: Node, pscope: NamespaceBinding, ind: Int){
+      node match {
+        case n: Elem if n.child.isEmpty => makeBox(ind, leafTag(n))
+        case _ => super.traverse(node, pscope, ind)
+      }
+    }
+  }
 
   private val Header = s"""<?xml version="1.0" encoding="$Encoding"?>"""
 
